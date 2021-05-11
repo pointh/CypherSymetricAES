524b9935e71978df6d479a5e387b87f036ea7a22
Pavel Svec <pavel.svec@itcrawl.cz>
2021-05-11 15:39:03 +0200

    Xml verze klíčů pro RSA

--- CypherAsymetricRSA/Program.cs
+++ CypherAsymetricRSA/Program.cs
@@ -8,10 +8,15 @@ namespace CypherAsymetricRSA
         static void Main(string[] args)
         {
             byte[] data = new byte[8];
+            
             RandomNumberGenerator.Create().GetBytes(data);
             WriteByteArray(data);
 
-            RSAParameters publicKey, privateKey;
+            // RSAParameters publicKey, privateKey;
+            // nebo
+            // xml string publicKey, privateKey
+
+            string publicKey, privateKey;
             GenerateKeys(2048, out publicKey, out privateKey);
 
             var encryptedData = Encrypt(data, publicKey);
@@ -24,32 +29,43 @@ namespace CypherAsymetricRSA
             Console.WriteLine(string.Join(", ", arr));
         }
 
-        static void GenerateKeys(int keyLength, out RSAParameters publicKey, out RSAParameters privateKey)
+        static void GenerateKeys(int keyLength, out string publicKey, out string privateKey)
+        // nebo 
+        //static void GenerateKeys(int keyLength, out RSAParameters publicKey, out RSAParameters privateKey)
         {
             using (var rsa = RSA.Create())
             {
                 rsa.KeySize = keyLength;
-                publicKey = rsa.ExportParameters(includePrivateParameters: false);
-                privateKey = rsa.ExportParameters(includePrivateParameters: true);
+                //publicKey = rsa.ExportParameters(includePrivateParameters: false);
+                //privateKey = rsa.ExportParameters(includePrivateParameters: true);
+                privateKey = rsa.ToXmlString(true);
+                publicKey = rsa.ToXmlString(false);
             }
         }
 
-        static byte[] Encrypt(byte[] data, RSAParameters publicKey)
+        static byte[] Encrypt(byte[] data, string publicKey)
+        // static byte[] Encrypt(byte[] data, RSAParameters publicKey)
         {
             using (var rsa = RSA.Create())
             {
-                rsa.ImportParameters(publicKey);
+                //rsa.ImportParameters(publicKey);
+
+                rsa.FromXmlString(publicKey);
 
                 var result = rsa.Encrypt(data, RSAEncryptionPadding.Pkcs1);
                 return result;
             }
         }
 
-        static byte[] Decrypt(byte[] data, RSAParameters privateKey)
+        static byte[] Decrypt(byte[] data, string privateKey)
+        // static byte[] Decrypt(byte[] data, RSAParameters privateKey)
         {
             using (var rsa = RSA.Create())
             {
-                rsa.ImportParameters(privateKey);
+                // rsa.ImportParameters(privateKey);
+
+                rsa.FromXmlString(privateKey);
+
                 return rsa.Decrypt(data, RSAEncryptionPadding.Pkcs1);
             }
         }
